b368bb09104bc7f720715a9a7e0fdfd29cb9e129
blayzeon <blayzeon@gmail.com>
2022-04-25 14:53:38 -0400

    add babel for js6 tests

--- src/controller.js
+++ src/controller.js
@@ -4,4 +4,28 @@ function findAccount(accountNumber){
     return data.bna.find(numbers => numbers.account === accountNumber);
 }
 
-module.exports = findAccount;
+function returnBna(account){
+    return {
+        name1: account.name1,
+        name2: account.name2,
+        address1: account.address1,
+        address2: account.address2,
+        zip: account.zip,
+        city: account.city,
+        state: account.state,
+        phone1: account.phone1,
+        phone2: account.phone2,
+        email: account.email,
+        tax: account.tax,
+        passcode: account.passcode,
+        notes: account.notes,
+        au: account.au,
+        lec: account.lec,
+        facility: account.facility,
+    }
+}
+
+export {
+    findAccount,
+    returnBna
+}
--- tests/controller.test.js
+++ tests/controller.test.js
@@ -1,4 +1,7 @@
-const findAccount = require('../src/controller');
+import {
+    findAccount,
+    returnBna
+} from '../src/controller.js';
 
 test('checks if 8004838314 is an existing account', ()=> {
     expect(findAccount('8004838314')).toEqual(
@@ -8,4 +11,11 @@ test('checks if 8004838314 is an existing account', ()=> {
 
 test('checks if 0 is an existing account', ()=> {
     expect(findAccount('0')).toEqual(undefined);
+});
+
+test('Checks if we can get the BNA of an account', ()=> {
+    const account = findAccount('8004838314');
+    expect(returnBna(account)).toEqual(
+        expect.objectContaining({name1: 'GTL'}),
+    );
 });
