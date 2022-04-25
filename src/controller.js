65f15dd0594d28b3d69037d063539eaea83ca103
blayzeon <blayzeon@gmail.com>
2022-04-25 15:07:12 -0400

    create data obj

--- src/controller.js
+++ src/controller.js
@@ -1,31 +1,30 @@
-const data = require('./data.json');
+const jsonData = require('./data.json');
 
-function findAccount(accountNumber){
-    return data.bna.find(numbers => numbers.account === accountNumber);
-}
-
-function returnBna(account){
-    return {
-        name1: account.name1,
-        name2: account.name2,
-        address1: account.address1,
-        address2: account.address2,
-        zip: account.zip,
-        city: account.city,
-        state: account.state,
-        phone1: account.phone1,
-        phone2: account.phone2,
-        email: account.email,
-        tax: account.tax,
-        passcode: account.passcode,
-        notes: account.notes,
-        au: account.au,
-        lec: account.lec,
-        facility: account.facility,
+const data = {
+    all: jsonData,
+    find(accountNumber) { return this.all.bna.find(numbers => numbers.account === accountNumber) },
+    bna(account) {
+        return {
+            name1: account.name1,
+            name2: account.name2,
+            address1: account.address1,
+            address2: account.address2,
+            zip: account.zip,
+            city: account.city,
+            state: account.state,
+            phone1: account.phone1,
+            phone2: account.phone2,
+            email: account.email,
+            tax: account.tax,
+            passcode: account.passcode,
+            notes: account.notes,
+            au: account.au,
+            lec: account.lec,
+            facility: account.facility,
+        }
     }
 }
 
 export {
-    findAccount,
-    returnBna
+    data
 }
--- tests/controller.test.js
+++ tests/controller.test.js
@@ -1,21 +1,20 @@
 import {
-    findAccount,
-    returnBna
+    data
 } from '../src/controller.js';
 
 test('checks if 8004838314 is an existing account', ()=> {
-    expect(findAccount('8004838314')).toEqual(
+    expect(data.find('8004838314')).toEqual(
         expect.objectContaining({account: '8004838314'}),
     );
 });
 
 test('checks if 0 is an existing account', ()=> {
-    expect(findAccount('0')).toEqual(undefined);
+    expect(data.find('0')).toEqual(undefined);
 });
 
 test('Checks if we can get the BNA of an account', ()=> {
-    const account = findAccount('8004838314');
-    expect(returnBna(account)).toEqual(
+    const account = data.find('8004838314');
+    expect(data.bna(account)).toEqual(
         expect.objectContaining({name1: 'GTL'}),
     );
 });
